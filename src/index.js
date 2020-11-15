728542b1dc0609214ab49c814f0a901c73729dd6
Joao Gui <joaoguidev@gmail.com>
2020-11-15 11:08:43 -0800

    Refactored the code on index

--- src/index.js
+++ src/index.js
@@ -12,44 +12,24 @@ const CANVAS_HEIGTH = TOTALTILESON_Y * TILE_HEIGTH; //px
 //Setting the canvas size based on the map size
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGTH;
-
-var currentSecond = 0, frameCount = 0, framesLastSecond = 0;//scsddddsfdsefwaefasdvSdvsadvsagsgsafgd
 context.font = "bold 10pt sans-serif";
 
-
+//==============================Game Start============================
 let game = new Game(CANVAS_WIDTH, CANVAS_HEIGTH, TILE_WIDTH, TILE_HEIGTH, TOTALTILESON_X, TOTALTILESON_Y);
-
-
 game.start();
-
-
 
 
 //==============================Game Loop============================
 function gameLoop(timeStamp) {
-    var sec = Math.floor(Date.now()/1000);
-    if(sec!=currentSecond)
-	{
-		currentSecond = sec;
-		framesLastSecond = frameCount;
-		frameCount = 1;
-	}
-	else { frameCount++; }
     //Clear canvas for current frame
     context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGTH);
     //deltaTime: Amount of time elapsed since last frame. It is important to update positions based on the elapsed time.
     let deltaTime = timeStamp - lastTime;
     lastTime = timeStamp;
-    
     //==========Update and draw the game in here============
     game.update(deltaTime);
     game.draw(context);
-
-    //===================================================
-
-
-    context.fillStyle = "green";
-	context.fillText("FPS: " + framesLastSecond, 10, 20);
+    //Loop again
     window.requestAnimationFrame(gameLoop);
 }
 //Start Game Loop
