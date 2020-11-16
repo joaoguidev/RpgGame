1d2f10a8ea0c7d9057ac76077d8f9a32ce4d51dd
Joao Gui <joaoguidev@gmail.com>
2020-11-15 22:21:13 -0800

    terrain classes created and linked with the map

---
+++ src/terrainTiles/roadNW.js
@@ -0,0 +1,22 @@
+"use strict";
+import TerrainTile from "./terrainTile.js";
+
+export default class RoadNW extends TerrainTile{
+    constructor(map, currentPositionX, currentPositionY, currentCoordinateX, currentCoordinateY) {
+        super(map, currentPositionX, currentPositionY, currentCoordinateX, currentCoordinateY)
+        this.walkable = true;
+        this.texture = new Image();
+        this.texture.src= './textures/Tile/medievalTile_32.png'
+    }
+
+    draw(context) {
+        context.drawImage(this.texture, this.position.x, this.position.y, this.tileWidth,this.tileHeigth);
+    }
+
+    //Update the position taking in consideration the elapsed time since the last frame. This makes the movement coherent with processing in different speeds. 
+    update(deltaTime){
+        if(!deltaTime) {
+            return;
+        }
+    }
+}
